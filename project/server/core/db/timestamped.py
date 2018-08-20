c42ce88d38caebb20f354921816e30433af18b36
= <=>
2018-08-20 16:12:37 +0300

    temp

---
+++ project/server/core/db/timestamped.py
@@ -0,0 +1,17 @@
+class TimeStamped(models.Model):
+    """
+    Provides created and updated timestamps on models.
+    """
+
+    class Meta:
+        abstract = True
+
+    created = models.DateTimeField(null=True, editable=False)
+    updated = models.DateTimeField(null=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        _now = now()
+        self.updated = _now
+        if not self.id:
+            self.created = _now
+        super(TimeStamped, self).save(*args, **kwargs)
