9e4524121cd408fed930de14d43542c3f5d95483
= <=>
2018-09-05 21:49:08 +0300

    111

--- project/client/src/instances/loginForm.ts
+++ project/client/src/instances/loginForm.ts
@@ -1,6 +1,12 @@
+import Vue from "vue"
+import Element from "element-ui"
+
+Vue.use(Element)
+
 new Vue({
-    el: '#login-form',
+    el: "#login-form",
     created() {
+        console.log(Element);
         console.log("CREATED");
     }
 });
--- project/client/src/loginPage.ts
+++ project/client/src/loginPage.ts
@@ -1,9 +1,11 @@
-// import loginForm from "./instances/loginForm"
+import Vue from "vue"
+import Element from "element-ui"
+import "element-ui/lib/theme-chalk/index.css";
 
-import Vue from 'vue';
+Vue.use(Element)
 
 new Vue({
-    el: '#login-form',
+    el: "#login-form",
     created() {
         console.log("CREATED");
     }
