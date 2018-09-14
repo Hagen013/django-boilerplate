9c6c7a4b84700be005a49159726224c89b9f5dba
= <=>
2018-09-14 19:25:00 +0300

    115

--- project/server/api/v0/urls.py
+++ project/server/api/v0/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 app_name = "api"
 
 urlpatterns = [
-    path("users/", include("api.v1.users.urls", namespace="users")),
+    path("users/", include("api.v0.users.urls", namespace="users")),
 ]
